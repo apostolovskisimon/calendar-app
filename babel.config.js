9772d7ddc9bf6996b124c80ec6201ba71dfd3592
Simon Apostolovski <apostolovskisimon@gmail.com>
2024-10-20 13:57:07 +0200

    install dependecies, firebase setup

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,25 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@/components': './src/components',
+          '@/pages': './src/pages',
+          '@/views': './src/views',
+          '@/App': './src/App',
+          '@/assets': './src/assets',
+          '@/contexts': './src/contexts',
+          '@/screens': './src/screens',
+          '@/styles': './src/styles',
+          '@/routes': './src/routes',
+          '@/services': './src/services',
+          '@/hooks': './src/hooks',
+          '@/shared': './src/shared',
+        },
+      },
+    ],
+  ],
 };
